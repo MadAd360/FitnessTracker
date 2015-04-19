c1f9e6bb02774c5af68c9522e97b465b2d741bcc
Adam <madad3000@aol.com>
2015-04-19 11:33:34 +0100

    Latest bug fixes

--- app/src/main/java/com/uni/ard/fitnesstracker/ChallengeView.java
+++ app/src/main/java/com/uni/ard/fitnesstracker/ChallengeView.java
@@ -120,8 +120,8 @@ public class ChallengeView extends Fragment {
     }
 
     public void updateView(){
-        mOpponentText.setText(opponentName);
-        mPenaltyText.setText(penalty);
+        mOpponentText.setText(opponentName + "");
+        mPenaltyText.setText(penalty + "");
         if(lost){
             mOutcomeText.setText("Lost");
             mOutcomeButton.setVisibility(View.GONE);
